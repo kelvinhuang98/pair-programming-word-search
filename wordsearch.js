6bf1e08dd823f3be73f9f13641bdbcb555388312
kelvinhuang98 <kelvin.huang98@hotmail.com>
2022-10-26 23:35:08 -0400

    Changes

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,20 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const transpose = require("../../async/matrix_transposition");
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+  const verticalJoin = transpose(letters).map((xs) => xs.join(""));
+  const horizontalJoin = letters.map((ls) => ls.join(""));
+  for (l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
+    } else {
+      for (x of verticalJoin) {
+        if (x.includes(word)) {
+          return true;
+        }
+      }
+    }
+  }
+  return false;
+};
+
+module.exports = wordSearch;
